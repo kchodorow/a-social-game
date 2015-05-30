63f3f7dce997071b360aec91d079515cc949ba9c
Kristina Chodorow <k.chodorow@gmail.com>
2015-05-29 23:21:38 -0400

    Timers working

--- src/main/webapp/js/social.js
+++ src/main/webapp/js/social.js
@@ -1,10 +1,10 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update : update });
 
-var text;
+var tiles = [];
 var counter = 0;
 
 function preload () {
-    game.load.image('einstein', 'assets/mushroom.jpg');
+  ;  game.load.image('einstein', 'assets/mushroom.jpg');
 }
 
 function create() {
@@ -13,8 +13,13 @@ function create() {
 
     for (var i = game.world.centerX - width * 2; i <= game.world.centerX + width * 2; i += width) {
         for (var j = game.world.centerY - height * 2; j <= game.world.centerY + height * 2; j += height) {
-            var tile = new Tile(game.add.sprite(i, j, 'einstein'));
+            tiles.push(new Tile(game.add.sprite(i, j, 'einstein')));
         }
     }
-    text = game.add.text(250, 16, '', { fill: '#ffffff' });
-}
+};
+
+function update() {
+    for (var i = 0; i < tiles.length; ++i) {
+        tiles[i].update();
+    }
+};
--- src/main/webapp/js/tile.js
+++ src/main/webapp/js/tile.js
@@ -5,6 +5,10 @@ var Tile = function(sprite) {
     sprite.height = Tile.HEIGHT;
     sprite.inputEnabled = true;
     sprite.events.onInputDown.add(this.listener, this);
+
+    this.level_ = 0;
+    this.timer_ = game.time.create(false);
+    this.timer_.add(11000, this.finish_, this);
 };
 
 Tile.WIDTH = 44;
@@ -15,7 +19,24 @@ Tile.prototype.getSprite = function() {
 };
 
 Tile.prototype.listener = function() {
-    counter++;
+    if (this.timer_.running) {
+        return;
+    }
+    this.text_ = game.add.text(this.sprite_.x, this.sprite_.y, '', { fill: '#ffffff' });
+    this.text_.anchor.set(0.5);
+    this.timer_.start();
+
+    this.level_++;
     this.sprite_.tint = 0xff00ff;
-    text.text = "You clicked " + counter + " times!";
+};
+
+Tile.prototype.update = function() {
+    if (this.timer_.running) {
+        this.text_.text = Math.floor(this.timer_.ms / 1000);
+    }
+};
+
+Tile.prototype.finish_ = function() {
+    console.log("done");
+    this.timer_.stop();
 };
