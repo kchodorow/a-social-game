dae032548f22543c94889e900de8fa108562967d
Kristina Chodorow <k.chodorow@gmail.com>
2015-05-30 17:39:00 -0400

    Use sprite sheet

--- src/main/webapp/js/social.js
+++ src/main/webapp/js/social.js
@@ -4,16 +4,18 @@ var tiles = [];
 var counter = 0;
 
 function preload () {
-  ;  game.load.image('einstein', 'assets/mushroom.jpg');
+    game.load.image('einstein', 'assets/mushroom.jpg');
+    game.load.spritesheet('ms', 'assets/spritesheet.png', 16, 16);
 }
 
 function create() {
-    var width = 44;
-    var height = 44;
+    var width = 64;
+    var height = 64;
+    var frames = game.cache.getFrameData('ms').getFrames();
 
     for (var i = game.world.centerX - width * 2; i <= game.world.centerX + width * 2; i += width) {
         for (var j = game.world.centerY - height * 2; j <= game.world.centerY + height * 2; j += height) {
-            tiles.push(new Tile(game.add.sprite(i, j, 'einstein')));
+            tiles.push(new Tile(game.add.sprite(i, j, 'ms', 0)));
         }
     }
 };
--- src/main/webapp/js/tile.js
+++ src/main/webapp/js/tile.js
@@ -4,7 +4,9 @@ var Tile = function(sprite) {
     sprite.width = Tile.WIDTH;
     sprite.height = Tile.HEIGHT;
     sprite.inputEnabled = true;
-    sprite.events.onInputDown.add(this.listener, this);
+    sprite.events.onInputDown.add(this.listener_, this);
+    sprite.events.onInputOver.add(this.showTimer_, this);
+    sprite.events.onInputOut.add(this.hideTimer_, this);
 
     this.level_ = 0;
     this.timer_ = game.time.create(false);
@@ -12,28 +14,37 @@ var Tile = function(sprite) {
     this.text_ = game.add.text(this.sprite_.x, this.sprite_.y, '', { fill: '#ffffff' });
     this.text_.anchor.set(0.5);
     this.text_.visible = false;
+
+    // TODO: move to subclasses.
+    this.color_ = 0xffffff;
+    this.color_inc_ = 0x202000;
 };
 
-Tile.WIDTH = 44;
-Tile.HEIGHT = 44;
-
-Tile.prototype.getSprite = function() {
-    return this.sprite_;
-};
-
-Tile.prototype.listener = function() {
-    if (this.timer_.running) {
-        return;
-    }
-    this.text_.visible = true;
-    this.timer_.start();
-    this.sprite_.tint = 0xff00ff;
-};
+Tile.WIDTH = 64;
+Tile.HEIGHT = 64;
 
 Tile.prototype.update = function() {
     if (this.timer_.running) {
         this.text_.text = this.getLevelTime_() - Math.floor(this.timer_.ms / 1000);
     }
+};
+
+Tile.prototype.listener_ = function() {
+    if (this.timer_.running) {
+        return;
+    }
+    this.text_.visible = true;
+    this.timer_.start();
+    this.color_ -= this.color_inc_;
+    this.sprite_.tint = this.color_;
+};
+
+Tile.prototype.showTimer_ = function() {
+    this.text_.visible = true;
+};
+
+Tile.prototype.hideTimer_ = function() {
+    this.text_.visible = false;
 };
 
 Tile.prototype.getLevelTime_ = function() {
