35971194ef0ac86cccac7acc373c03f10cbb6424
Kristina Chodorow <k.chodorow@gmail.com>
2015-05-29 21:53:06 -0400

    Tile class working

--- src/main/webapp/js/social.js
+++ src/main/webapp/js/social.js
@@ -13,19 +13,8 @@ function create() {
 
     for (var i = game.world.centerX - width * 2; i <= game.world.centerX + width * 2; i += width) {
         for (var j = game.world.centerY - height * 2; j <= game.world.centerY + height * 2; j += height) {
-            var image = game.add.sprite(i, j, 'einstein');
-            image.anchor.set(0.5);
-            image.width = 44;
-            image.height = 44;
-            image.inputEnabled = true;
-            image.events.onInputDown.add(listener, image);
+            var tile = new Tile(game.add.sprite(i, j, 'einstein'));
         }
     }
     text = game.add.text(250, 16, '', { fill: '#ffffff' });
-}
-
-function listener () {
-    counter++;
-    this.tint = 0xff00ff;
-    text.text = "You clicked " + counter + " times!";
 }
--- src/main/webapp/js/tile.js
+++ src/main/webapp/js/tile.js
@@ -1,7 +1,21 @@
 var Tile = function(sprite) {
     this.sprite_ = sprite;
+    sprite.anchor.set(0.5);
+    sprite.width = Tile.WIDTH;
+    sprite.height = Tile.HEIGHT;
+    sprite.inputEnabled = true;
+    sprite.events.onInputDown.add(this.listener, this);
 };
+
+Tile.WIDTH = 44;
+Tile.HEIGHT = 44;
 
 Tile.prototype.getSprite = function() {
     return this.sprite_;
+};
+
+Tile.prototype.listener = function() {
+    counter++;
+    this.sprite_.tint = 0xff00ff;
+    text.text = "You clicked " + counter + " times!";
 };
