2574b6cb2ade8148fc55c142923c14c637d28fa9
Kristina Chodorow <k.chodorow@gmail.com>
2015-05-29 23:29:32 -0400

    Progressive timer counting down

--- src/main/webapp/js/tile.js
+++ src/main/webapp/js/tile.js
@@ -9,6 +9,9 @@ var Tile = function(sprite) {
     this.level_ = 0;
     this.timer_ = game.time.create(false);
     this.timer_.add(11000, this.finish_, this);
+    this.text_ = game.add.text(this.sprite_.x, this.sprite_.y, '', { fill: '#ffffff' });
+    this.text_.anchor.set(0.5);
+    this.text_.visible = false;
 };
 
 Tile.WIDTH = 44;
@@ -22,21 +25,24 @@ Tile.prototype.listener = function() {
     if (this.timer_.running) {
         return;
     }
-    this.text_ = game.add.text(this.sprite_.x, this.sprite_.y, '', { fill: '#ffffff' });
-    this.text_.anchor.set(0.5);
+    this.text_.visible = true;
     this.timer_.start();
-
-    this.level_++;
     this.sprite_.tint = 0xff00ff;
 };
 
 Tile.prototype.update = function() {
     if (this.timer_.running) {
-        this.text_.text = Math.floor(this.timer_.ms / 1000);
+        this.text_.text = this.getLevelTime_() - Math.floor(this.timer_.ms / 1000);
     }
 };
 
+Tile.prototype.getLevelTime_ = function() {
+    var time = [10, 100, 1000, 10000];
+    return time[this.level_];
+};
+
 Tile.prototype.finish_ = function() {
-    console.log("done");
     this.timer_.stop();
+    this.level_++;
+    this.text_.visible = false;
 };
